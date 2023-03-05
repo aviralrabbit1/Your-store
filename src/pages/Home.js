df5db89ca14715b4770797bfdd11fa05c6f2e685
aviralrabbit1 <aviral.robin@gmail.com>
2023-03-06 01:24:10 +0530

    Added home-wrapper 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -90,6 +90,126 @@ const Home = () => {
         </div>
         
       </section>
+      <section className='home-wrapper-2 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='services d-flex align-items-center justify-content-between'>
+                <div className='d-flex align-items-center gap-30'> 
+                  <img src="images/service.png" alt="services" />
+                  <div> {/* putting texts on top of each other */}                                        
+                    <h6>Free Shipping</h6>
+                    <p className='mb-0'>For all orders over Rs 500</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-30'> 
+                  <img src="images/service-02.png" alt="services" />
+                  <div>                    
+                    <h6>Daily offers!</h6>
+                    <p className='mb-0'>Save upto 50% off</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-30'> 
+                  <img src="images/service-03.png" alt="services" />
+                  <div>                    
+                    <h6>Support 24/7</h6>
+                    <p className='mb-0'>Shop with an expert</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-30'> 
+                  <img src="images/service-04.png" alt="services" />
+                  <div>                    
+                    <h6>Affordable Prices</h6>
+                    <p className='mb-0'>Get everything at real prices!</p>
+                  </div>
+                </div>
+                <div className='d-flex align-items-center gap-30'> 
+                  <img src="images/service-05.png" alt="services" />
+                  <div>                    
+                    <h6>Secure Payments</h6>
+                    <p className='mb-0'>100% safe</p>
+                  </div>
+                </div>
+                
+              </div>
+              
+            </div>
+            
+          </div>
+          
+        </div>
+        
+      </section>
+      <section className='home-wrapper-2 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12'>
+              <div className='categories d-flex flex-wrap justify-content-between align-items-center'>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>Camera</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>TV</h6>
+                    <p>12 items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>Watch</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>Camera</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>Camera</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>TV</h6>
+                    <p>12 items</p>
+                  </div>
+                  <img src="images/tv.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>Watch</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/headphone.jpg" alt="camera" />
+                </div>
+                <div className='d-flex gap-15 align-items-center'>
+                  <div>
+                    <h6>Camera</h6>
+                    <p>10 items</p>
+                  </div>
+                  <img src="images/camera.jpg" alt="camera" />
+                </div>
+                
+              </div>
+              
+            </div>
+            
+          </div>
+          
+        </div>
+      </section>
     </div>
   )
 }
