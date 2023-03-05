996ff3fa7b406cee11017c001b0043b2f408027f
aviralrabbit1 <aviral.robin@gmail.com>
2023-03-05 16:13:10 +0530

    Added top banner

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,51 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 
 const Home = () => {
   return (
     <div>
-      Home
+      <section className='home-wrapper-1 py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner position-relative p-3'>
+                <img className='img-fluid rounded-3' src="https://www.savincom.co.uk/data/themes/savincom/assets/images/blank-banner.png" 
+                alt="main banner" />
+                
+                <div className='main-banner-content position-absolute'>
+                  <div className='main-banner-content-info'>                  
+                  <h4>Supercharged for pros!</h4>
+                  <h5>ipad S13+ Pro</h5>
+                  <p>From 65000 Rs</p>
+                  <Link className='button'>BUY NOW!</Link>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+            <div className='col-6'>
+              <div className='d-flex flex-wrap justify-content-between align-items-center'>
+                <div className='small-banner position-relative p-3'>
+                    <img className='img-fluid rounded-3' 
+                    src="https://www.savincom.co.uk/data/themes/savincom/assets/images/blank-banner.png" 
+                    alt="main banner" 
+                    />                    
+                    <div className='small-banner-content position-absolute'>
+                      <div className='main-banner-content-info'>                  
+                      <h4>Supercharged for pros!</h4>
+                      <h5>ipad S13+ Pro</h5>
+                      <p>From 65000 Rs</p>
+                      <Link className='button'>BUY NOW!</Link>
+                      </div>
+                    </div>
+                  </div>                  
+                </div>            
+              </div>
+          </div>
+          
+        </div>
+        
+      </section>
     </div>
   )
 }
